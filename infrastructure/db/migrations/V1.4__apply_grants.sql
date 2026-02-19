ff7f145e6ddaebe8ea5a285cffe184347958928e
Lorenzo Fratini <86708730+lorenzofratini1998@users.noreply.github.com>
2026-02-19 21:04:41 +0100

    feat(ci-cd): added ci pipelines (#23)
    - Added CI/CD pipelines
    - Updated docker-compose with microservices
    - Added README.md
    
    Closes #22

--- infrastructure/db/migrations/V1.4__apply_grants.sql
+++ infrastructure/db/migrations/V1.4__apply_grants.sql
@@ -1,17 +1,17 @@
 -- GRANT on schemas
-GRANT USAGE ON SCHEMA video_domain TO ms_orchestrator;
-GRANT USAGE ON SCHEMA video_domain TO ms_policy_engine;
-GRANT USAGE ON SCHEMA analysis_domain TO ms_orchestrator;
-GRANT USAGE ON SCHEMA analysis_domain TO ms_policy_engine;
+GRANT USAGE ON SCHEMA video_domain TO ${MS_ORCHESTRATOR_USER};
+GRANT USAGE ON SCHEMA video_domain TO ${MS_POLICY_ENGINE_USER};
+GRANT USAGE ON SCHEMA analysis_domain TO ${MS_ORCHESTRATOR_USER};
+GRANT USAGE ON SCHEMA analysis_domain TO ${MS_POLICY_ENGINE_USER};
 
 -- Video Orchestrator
-GRANT SELECT, INSERT, UPDATE, DELETE ON video_domain.videos TO ms_orchestrator;
-GRANT SELECT, INSERT, UPDATE, DELETE ON video_domain.analysis_jobs TO ms_orchestrator;
-GRANT SELECT, INSERT, UPDATE, DELETE ON video_domain.brand_profiles TO ms_orchestrator;
-GRANT SELECT, INSERT, UPDATE, DELETE ON video_domain.video_contents TO ms_orchestrator;
-GRANT SELECT ON analysis_domain.analysis_results TO ms_orchestrator;
+GRANT SELECT, INSERT, UPDATE, DELETE ON video_domain.videos TO ${MS_ORCHESTRATOR_USER};
+GRANT SELECT, INSERT, UPDATE, DELETE ON video_domain.analysis_jobs TO ${MS_ORCHESTRATOR_USER};
+GRANT SELECT, INSERT, UPDATE, DELETE ON video_domain.brand_profiles TO ${MS_ORCHESTRATOR_USER};
+GRANT SELECT, INSERT, UPDATE, DELETE ON video_domain.video_contents TO ${MS_ORCHESTRATOR_USER};
+GRANT SELECT ON analysis_domain.analysis_results TO ${MS_ORCHESTRATOR_USER};
 
 -- Policy Engine
-GRANT SELECT, INSERT, UPDATE, DELETE ON analysis_domain.analysis_results TO ms_policy_engine;
-GRANT SELECT, INSERT, UPDATE, DELETE ON analysis_domain.policy_versions TO ms_policy_engine;
-GRANT SELECT ON video_domain.video_contents TO ms_policy_engine;
+GRANT SELECT, INSERT, UPDATE, DELETE ON analysis_domain.analysis_results TO ${MS_POLICY_ENGINE_USER};
+GRANT SELECT, INSERT, UPDATE, DELETE ON analysis_domain.policy_versions TO ${MS_POLICY_ENGINE_USER};
+GRANT SELECT ON video_domain.video_contents TO ${MS_POLICY_ENGINE_USER};
